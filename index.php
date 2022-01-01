674fabee72c0eea642847957322b45c1673ea57e
Ryan J. Williams <ryan.j.williams.0@gmail.com>
2022-01-01 18:45:42 -0500

    First changes, testing phase 1.

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-phpinfo();
-?>
+<html>
+<head>
+    <title>Star Odyssey</title>
+    <link rel="stylesheet" href="css/external.css">
+</head>
+<body>
+
+<h1>This is a test.</h1>
+<div>This is another test.</h1>
+
+
+</body>
+</html>
