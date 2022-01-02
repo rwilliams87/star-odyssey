060062bdd0c0a401d78092bb3476bb479843151f
Ryan J. Williams <ryan.j.williams.0@gmail.com>
2022-01-02 15:48:40 -0500

    Splash page made, animations set.

--- index.php
+++ index.php
@@ -5,9 +5,69 @@
 </head>
 <body>
 
-<h1>This is a test.</h1>
-<div>This is another test.</h1>
+<!-- Main Grid Starts Here. Nothing outside of this. -->
+<div class="maingrid">
+<!-- Top Area. Header Is Here. -->
+<div class="toparea">
+    Star Odyssey
+</div>
+<!-- Center Area. Will Be Used As A Sign-In -->
+<div class="centerarea">
 
+<!-- Sign In Form -->
+
+<!--
+<form>
+<div class="signin">
+    <div class="signin-header"> Sign In </div>
+    <div class="unametext"> Username: </div>
+    <div class="unamefield"> <input type="text" class="formfield" placeholder="Username..."> </div>
+    <div class="passtext"> Password: </div>
+    <div class="passfield"> <input type="text" class="formfield" placeholder="Password..."> </div>
+    <div class="buttonleft"> </div>
+    <div class="buttonright"> <input type="submit" class="button" value="Submit"></div>
+    <div class="signin-bottom"> <a href="signup.php">Need An Account?</a> | <a href="forgotpassword.php">Forgot Your Password?</a> </div>
+</div>
+</form>
+-->
+
+
+<table width="500px">
+    <tr>
+        <td colspan="2" class='theader'>Sign In</td>
+    </tr>
+    <tr>
+        <td class='rowheader' width='30%'> Username: </td>
+        <td><input type='text' class='formfield' placeholder='Username...'></td>
+    </tr>
+    <tr>
+        <td class='rowheader'>Password: </td>
+        <td><input type='text' class='formfield' placeholder='Password...'></td>
+    </tr>
+    <tr>
+        <td colspan="2" class='colheader'><input type='submit' value='Submit' class='form'></td>
+    </tr>
+</table>
+
+
+</div>
+<!-- Below Sign-In Area -->
+<div class="bottomarea">
+<div class="colorgrid">
+    <div class="cbox01"> </div>
+    <div class="cbox02"> </div>
+    <div class="cbox03"> </div>
+    <div class="cbox04"> </div>
+    <div class="cbox05"> </div>
+    <div class="tbox01"> #C1CDD5 </div>
+    <div class="tbox02"> #111133 </div>
+    <div class="tbox03"> #000000 </div>
+    <div class="tbox04"> #333355 </div>
+    <div class="tbox05"> #222233 </div>
+</div>
+
+<!-- End Main Grid. Nothing Outside Of This. -->
+</div>
 
 </body>
 </html>
---
+++ splash.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Star Odyssey</title>
+    <link rel='stylesheet' href='css/splash.css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <div id='stars'></div>
+    <div id='stars2'></div>
+    <div id='stars3'></div>
+    <div class='container'>
+        <div class='formwrapper'>
+            <h1 class='letterspacing'>Star Odyssey</h1>
+            <h3>Please Enter Your Account Credentials</h3>
+            <input type='text' placeholder='Username...' class='textbox'>
+            <div class='spacing'></div>
+            <input type='password' placeholder='Password...' class='textbox'>
+            <div class='spacing'></div>
+            <input type='submit' value='Log In' class='button'>
+            <div class='spacing'></div>
+            <div class='spacing'></div>
+            <div class='centering'>
+                <a href='#'>Sign Up</a>
+            </div>
+            <div class='spacing'></div>
+            <div class='centering'>
+                <a href='#'>Forgot Your Password?</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
